ee81fe6697e1db191fc797236d9a3b07a910aa21
karina-ranadive <karinaranadive@gmail.com>
2021-07-21 11:09:22 -0400

    initial commit

--- map.h
+++ map.h
@@ -1,1 +1,16 @@
 #include <map>
+#include <iostream>
+using namespace std;
+
+class MapImplentation{
+    private:
+        //map objects
+        //map iterator
+    public:
+        void SearchRegion();
+        void SearchName();
+        void SearchCause();
+        void UpdateMatchIndex();
+        void SearchMultipleOptions();
+
+};

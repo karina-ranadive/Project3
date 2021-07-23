2f547d28de2569d1b00e390c51251109672bb43a
Katie Hartley <314katherine@gmail.com>
2021-07-23 16:35:53 -0400

    Added code to read in and organize data

--- main.cpp
+++ main.cpp
@@ -1,3 +1,150 @@
-int main(){
+#include MapClass.h
+#include Nonprofit.h
+#include <fstream>
+#include <sstream>
 
+int main(){
+	MapClass a = MapClass("Arts, Culture & Humanities");
+	MapClass b = MapClass("Education");
+	MapClass cd = MapClass("Environment and Animals");
+	MapClass eh = MapClass("Health");
+	MapClass ip = MapClass("Human Services");
+	MapClass q = MapClass("International, Foreign Affairs");
+	MapClass rw = MapClass("Public, Societal Benefit");
+	MapClass x = MapClass("Religion Related");
+	MapClass y = MapClass("Mutual/Membership Benefit");
+
+	ifstream inFile("NPOMasterFile.csv");
+
+	if (inFile.is_open()) {
+		string lineFromFile;
+		getline(inFile, lineFromFile);
+
+		while (getline(inFile, lineFromFile)) {
+			istringstream stream(lineFromFile);
+
+			string name;
+			string street;
+			string zipStr;
+			string state;
+			string regionStr;
+			string NTEE;
+			int zip;
+			int region;
+			int revenue;
+
+			getline(stream, name, ',');
+			getline(stream, street, ',');
+			getline(stream, zipStr, ',');
+			getline(stream, state, ',');
+			getline(stream, regionStr, ',');
+			getline(stream, NTEE);
+
+			zip = stoi(zipStr);
+			region = stoi(regionStr);
+
+			Nonprofit n(name, street, zip, state, region, NTEE);
+			 
+			if (NTEE.substr(0, 1).compare("A") == 0) {
+				n.setSubcat("Arts, Culture & Humanties");
+				a.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("B") == 0) {
+				n.setSubcat("Education");
+				b.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("C") == 0) {
+				n.setSubcat("Environment");
+				cd.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("D") == 0) {
+				n.setSubcat("Animal-Related");
+				cd.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("E") == 0) {
+				n.setSubcat("Health Care");
+				eh.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("F") == 0) {
+				n.setSubcat("Mental Health & Crisis Intervention");
+				eh.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("G") == 0) {
+				n.setSubcat("Voluntary Health Associations & Medical Discipliness");
+				eh.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("H") == 0) {
+				n.setSubcat("Medical Research");
+				eh.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("I") == 0) {
+				n.setSubcat("Crime & Legal-Related");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("J") == 0) {
+				n.setSubcat("Employment");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("K") == 0) {
+				n.setSubcat("Food, Agriculture & Nutrition");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("L") == 0) {
+				n.setSubcat("Housing & Shelter");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("M") == 0) {
+				n.setSubcat("Public Safety, Disaster Preparedness & Relief");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("N") == 0) {
+				n.setSubcat("Recreation & Sports");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("O") == 0) {
+				n.setSubcat("Youth Development");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("P") == 0) {
+				n.setSubcat("Human Services");
+				ip.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("Q") == 0) {
+				n.setSubcat("International, Foreign Affairs & National Security");
+				q.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("R") == 0) {
+				n.setSubcat("Civil Rights, Social Action & Advocacy");
+				rw.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("S") == 0) {
+				n.setSubcat("Community Improvement & Capacity Building");
+				rw.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("T") == 0) {
+				n.setSubcat("Philanthropy, Voluntarism & Grantmaking Foundations");
+				rw.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("U") == 0) {
+				n.setSubcat("Science & Technology");
+				rw.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("V") == 0) {
+				n.setSubcat("Social Science");
+				rw.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("W") == 0) {
+				n.setSubcat("Public & Societal Benefit");
+				rw.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("X") == 0) {
+				n.setSubcat("Religion-Related");
+				x.InsertIntoMap(name, n);
+			}
+			else if (NTEE.substr(0, 1).compare("Y") == 0) {
+				n.setSubcat("Mutual & Membership Benefit");
+				y.InsertIntoMap(name, n);
+			}
+		}
+	} 
 }

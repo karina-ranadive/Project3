d9a5882875862780115d5b76068848d6498fd846
Katie Hartley <314katherine@gmail.com>
2021-07-28 18:12:33 -0400

    Variable Updates

--- Tree.h
+++ Tree.h
@@ -1,3 +1,8 @@
 class Tree {
-
+    Nonprofit *root;
+    Nonprofit target;
+public:
+    Tree(Nonprofit target, map<string, Nonprofit>& orgs);
+    void CalculateMatch(Nonprofit& comp);
+    bool IsEmpty();
 };

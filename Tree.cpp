45ec495351db2553ef43bd0c5bb1ccd7dc7322d8
karina-ranadive <karinaranadive@gmail.com>
2021-07-28 18:58:14 -0400

    match index

--- Tree.cpp
+++ Tree.cpp
@@ -11,3 +11,18 @@ Nonprofit* Tree::Insert(Nonprofit* root, Nonprofit *obj){
         root->right = Insert(root->right, obj);
     return root;
 }
+
+void Tree::CalculateMatch(Nonprofit& comp) {
+	int match = 0;
+	if (target.getRegion() != -1 && target.getRegion() == comp.getRegion())
+		match++;
+	if (target.getState() != "" && target.getState() == comp.getState())
+		match++;
+	if (target.getStreet() != "" && target.getStreet() == comp.getStreet())
+		match++;
+	if (target.getZip() != -1 && target.getZip() == comp.getZip())
+		match++;
+	if (target.getSubCat() != "" && target.getSubCat() == comp.getSubCat())
+		match++;
+	comp.matchIndex = match;
+}

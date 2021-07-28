dc9de12b79a8e84dfb1911e9a9f8bd8c1f67771d
Katie Hartley <314katherine@gmail.com>
2021-07-28 18:59:05 -0400

    Functions and Constructor

--- Tree.cpp
+++ Tree.cpp
@@ -1,8 +1,14 @@
 #include "Tree.h"
 #include "Nonprofit.h"
 
-Nonprofit* Tree::Insert(Nonprofit* root, Nonprofit *obj){
+Tree(Nonprofit target, map<string, Nonprofit>& orgs) {
+    for (auto iter = orgs.begin(); iter != orgs.end(); iter++) {
+        CalculateMatch(iter->second);
+        Insert(iter->second);
+    }
+}
 
+Nonprofit* Tree::Insert(Nonprofit* root, Nonprofit *obj) {
     if(root == nullptr)
         return obj;
     else if(obj->matchIndex < root->matchIndex)
@@ -11,3 +17,12 @@ Nonprofit* Tree::Insert(Nonprofit* root, Nonprofit *obj){
         root->right = Insert(root->right, obj);
     return root;
 }
+
+void Tree::traverse(Nonprofit* root) {
+    if (root != nullptr) {
+        traverse(root->right);
+        root->printNonprofit;
+        traverse(root->left);
+    }
+}
+

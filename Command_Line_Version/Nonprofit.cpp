9df940eeeaf2c56ee2bfbe7469efa578011b4f14
krhartley <73544421+krhartley@users.noreply.github.com>
2021-08-02 15:25:06 -0400

    comments

--- Command_Line_Version/Nonprofit.cpp
+++ Command_Line_Version/Nonprofit.cpp
@@ -1,5 +1,6 @@
 #include "Nonprofit.h"
 
+// Constructor to assign all necessary class variables
 Nonprofit::Nonprofit(std::string name, std::string street, std::string zip, std::string state, int region, std::string NTEE) {
     this->name = name;
     this->street = street;
@@ -17,6 +18,7 @@ Nonprofit::Nonprofit(){
 
 }
 
+// Getter functions
 std::string Nonprofit::getStreet() {
     return street;
 }
@@ -45,12 +47,14 @@ std::string Nonprofit::getSubCat(){
     return this->subcategory;
 }
 
+// Prints nonprofit info
 void Nonprofit::printNonprofit() {
     std::cout << "Name: " << name << std::endl;
     std::cout << "Street address: " << street << " " << state << " " << zip << std::endl;
     std::cout << std::endl;
 }
 
+// Setter functions
 void Nonprofit::setSubcat(std::string subcategory) {
     this->subcategory = subcategory;
 }
@@ -64,10 +68,12 @@ void Nonprofit::setSubcatNum(int subcatNum) {
     this->subcatNum = subcatNum;    //create a map for subcat nums -> subcat names
 }
 
+// Last getter function
 std::string Nonprofit::getName() {
     return name;
 }
 
+// Overwrites for comparisons
 bool operator < (const Nonprofit& lhs, const Nonprofit& rhs) {
     return lhs.matchIndex < rhs.matchIndex;
 }

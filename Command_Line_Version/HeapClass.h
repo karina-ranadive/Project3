90c0c0b08fe6fb9737f21a26b6bdabccd87d86d3
klarubbio <78495457+klarubbio@users.noreply.github.com>
2021-08-02 16:19:03 -0400

    comments

--- Command_Line_Version/HeapClass.h
+++ Command_Line_Version/HeapClass.h
@@ -7,11 +7,14 @@
 using namespace std;
 
 class HeapClass {
+    //class member variables
     vector<Nonprofit> heap;
     Nonprofit target;
     int end_index;
 public:
+    //constructor
     HeapClass(Nonprofit target, map<string, Nonprofit>& orgs);
+    //Heap methods
     void HeapifyUp(Nonprofit add);
     void HeapifyDown(int i);
     Nonprofit GetMax();
